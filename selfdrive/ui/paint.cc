d398e15f26cd92777c741a09bab83bc0e9a48057
lispain <68838103+lispain@users.noreply.github.com>
2020-12-22 07:22:08 +0900

    Update paint.cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -440,7 +440,7 @@ static void ui_draw_standstill(UIState *s) {
     nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_BASELINE);
     nvgFontSize(s->vg, 60);
     nvgFillColor(s->vg, COLOR_ORANGE_ALPHA(230));
-    ui_print(s, viz_standstill_x+665, viz_standstill_y+300, "정차중");
+    ui_print(s, viz_standstill_x+660, viz_standstill_y+300, "정차중");
     
   }
 }
